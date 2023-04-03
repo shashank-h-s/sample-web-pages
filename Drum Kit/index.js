e9bd67618a04e9f015e376a8a7e72f1b316bc6fa
shashank-h-s <shashankhsappu@gmail.com>
2023-04-03 19:04:22 +0530

    Adding Drum kit-Starting file

---
+++ Drum Kit/index.js
@@ -0,0 +1,17 @@
+var buttonList = document.querySelectorAll(".drum");
+var audio = new Audio("sounds/tom-1.mp3");
+for (let i = 0; i < buttonList.length; i++) {
+  buttonList[i].addEventListener("click", handClick);
+}
+function handClick() {
+//   audio.play();
+var innerHtml = 
+    switch () {
+        case value:
+            
+            break;
+    
+        default:
+            break;
+    }
+}
