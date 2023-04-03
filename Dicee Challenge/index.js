e9ac2eb109619fe973ef791ab12452bf2f06086a
shashank-h-s <shashankhsappu@gmail.com>
2023-04-03 19:02:48 +0530

    adding dicee challenge

---
+++ Dicee Challenge/index.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomImage1 = "images/dice" + randomNumber1 + ".png";
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImage1);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImage2 = "images/dice" + randomNumber2 + ".png";
+var Image2 = document.querySelectorAll("img")[1];
+Image2.setAttribute("src", randomImage2);
+
+if (randomNumber1 === randomNumber2) {
+  document.querySelector("h1").innerHTML = "Draw";
+} else if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩Player 1 winner";
+} else {
+  document.querySelector("h1").innerHTML = "Player 2 🚩winner";
+}
