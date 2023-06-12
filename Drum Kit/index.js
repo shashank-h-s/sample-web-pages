8fed8de538dc9926435d442b8b2232d2a694a83c
shashank-h-s <shashankhsappu@gmail.com>
2023-06-12 14:50:06 +0530

    simon game

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,17 +1,55 @@
 var buttonList = document.querySelectorAll(".drum");
-var audio = new Audio("sounds/tom-1.mp3");
+
 for (let i = 0; i < buttonList.length; i++) {
   buttonList[i].addEventListener("click", handClick);
 }
 function handClick() {
-//   audio.play();
-var innerHtml = 
-    switch () {
-        case value:
-            
-            break;
-    
-        default:
-            break;
-    }
+  //   audio.play();
+  var letters = this.innerHTML;
+  makeSound(letters);
+  buttonAnimation(letters);
+}
+
+document.addEventListener("keypress", function (event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      new Audio("sounds/tom-2.mp3").play();
+      break;
+    case "s":
+      new Audio("sounds/tom-3.mp3").play();
+      break;
+    case "d":
+      new Audio("sounds/tom-4.mp3").play();
+      break;
+    case "j":
+      new Audio("sounds/snare.mp3").play();
+      break;
+    case "k":
+      new Audio("sounds/crash.mp3").play();
+      break;
+    case "l":
+      new Audio("sounds/kick-bass.mp3").play();
+      break;
+
+    default:
+      console.log(letters);
+      break;
+  }
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
